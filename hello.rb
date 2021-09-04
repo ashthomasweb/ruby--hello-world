e79c88d8cdb629414d01088733413707d0b6ce3c
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-09-03 22:36:24 -0400

    Adds a humorous and broken math function
    Ruby does not detect number type in a string like js.

--- hello.rb
+++ hello.rb
@@ -6,4 +6,11 @@ puts "Please enter your name."
 name = gets.chop
 puts "Hi, #{name}! I'm Ruby!"
 
+puts "Let's do a math problem."
+puts "Please pick two numbers."
+num1 = gets.chop
+num2 = gets.chop
+puts "I'll add these up. It's #{num1 + num2}!"
+puts "We can learn more operators later, okay #{name}?"
+
 # END of document
