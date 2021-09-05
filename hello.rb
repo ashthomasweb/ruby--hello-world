d00f7c55015c9ec110c3218edd7f769b46a59faa
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-09-05 12:40:01 -0500

    Minor comment updates

--- hello.rb
+++ hello.rb
@@ -1,7 +1,7 @@
-# 'Hello World' in ruby. My first second programming language.
+# 'Hello World' in ruby, and basic method creation. 
 
+# outputs all operators and then checks division to see if it matches your favorite number.
 def my_first_method (parameter1, parameter2, parameter3 = $name)
-    
     puts parameter1 + parameter2
     puts parameter1 - parameter2
     puts parameter1 * parameter2
@@ -21,11 +21,12 @@ def ruby_respond (var1, var2 = $rubyFave)
     end
 end
 
+# begin dialogue
 puts "Hello, World!"
 
 puts "Please enter your name."
 $name = gets.chop
-puts "Hi, #{$name}, I'm Ruby! We are now friends!"
+puts "Hi, #{$name}, I'm Ruby! Aren't we now friends?"
 
 puts "Let's do a math problem."
 puts "Please pick two numbers."
@@ -34,20 +35,21 @@ num2 = gets.chop
 puts "I'll add these up. It's #{num1 + num2}!"
 puts "We can learn more operators later, okay #{$name}?"
 
+# type conversion
 num1 = num1.to_i
 num2 = num2.to_i
 
 print "Here is a string"
 print "Here is another string"
-
 puts "Did you learn about my newline gotcha #{$name}?"
 
-puts "What's your favorite number, #{$name}?"
+puts "#{$name}, what's your favorite number?"
 $rubyFave = gets.chop.to_i
 
-puts "How about a new operator? y/n?"
+puts "I can perform all of the operators, would you like to see? y/n?"
 userInput = gets.chop
 
+# control flow
 if userInput == "y" 
     puts "Coming up..."
     my_first_method num1, num2
